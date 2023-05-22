9baa3a6422297629c631d0e1df8ba0f8625cf936
Hrayr121 <hrayr.qocharyan115@gmail.com>
2023-05-23 03:40:15 +0400

    Starting Task 3

---
+++ Billiard/src/Simulations/Stadium.java
@@ -0,0 +1,12 @@
+package Simulations;
+
+public class Stadium {
+	public static void main(String[] args) {
+        double r = 1.0;  // Radius of the circle
+        double xcLeft = 0.0;  // x coordinate of the center of the left semicircle
+        double xcRight = 2.0;  // x coordinate of the center of the right semicircle
+        double L = 2.0;  // Length of the line connecting the semicircles
+        double delta = 1e-6;  // Small delta for deviation check
+        int reflections = 5; 
+}
+}
